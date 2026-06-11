bb92b8ca3192ec03462c97b7f6d8b5153c43a3c5
Pojkee <cbo6oda22@gmail.com>
2026-06-11 07:22:02 +0300

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,15 @@
-import pytest
 from app.restore_names import restore_names
+from unittest import mock
 
-# write your tests here
+
+@mock.patch("app.restore_names.restore_from_file")
+def test_first_name_equal_to_none() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Black",
+            "ful_name": "Bob Black"
+        }
+    ]
+    restore_names(users)
+    assert users[0]["first_name"] == "Bob"
